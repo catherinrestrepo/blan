caec5d15d298ba0a530a51d9ea99b04add4ddbf6
catherinrestrepo <caterin26-1@hotmail.com>
2017-03-20 16:09:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var lock = new Auth0Lock('Mozm80VuxJu1fdlyiDtk4HqZK2mrd9PI', 'blan.auth0.com');
+var btn_login = document.getElementById('btn-login');
+
+btn_login.addEventListener('click', function() {
+  lock.show();
+});
+lock.on("authenticated", function(authResult) {
+  lock.getProfile(authResult.idToken, function(error, profile) {
+    if (error) {
+      // Handle error
+      return;
+    }
+    localStorage.setItem('id_token', authResult.idToken);
+    // Display user information
+    show_profile_info(profile);
+  });
+});
+
